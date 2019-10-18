0d87e968e7d851de9f033acd878b03924f7c9b0f
Luoxin <luoxin.ttt@gmail.com>
2019-10-18 20:52:00 +0800

    更改node配置

--- node.py
+++ node.py
@@ -1,5 +1,4 @@
 class Node:
-
     # ip
     ip = ''
     # port
@@ -25,7 +24,8 @@ class V2ray(Node):
     camouflagePath = ''
     camouflageTls = ''
 
-    def __init__(self, ip, port, remark, security, uuid, alterId, network, camouflageType, camouflageHost, camouflagePath, camouflageTls):
+    def __init__(self, ip, port, remark, security, uuid, alterId, network, camouflageType, camouflageHost,
+                 camouflagePath, camouflageTls):
         super(V2ray, self).__init__(ip, port, remark, security)
         self.uuid = uuid
         self.alterId = alterId
@@ -37,294 +37,112 @@ class V2ray(Node):
 
     def formatConfig(self):
         v2rayConf = {
-            "policy": {
-                "system": {
-                    "statsInboundUplink": True,
-                    "statsInboundDownlink": True
+            'log': {
+                'access': None,
+                'error': None,
+                'loglevel': None
+            },
+            'inbound': {
+                'port': 1086,
+                'listen': '127.0.0.1',
+                'protocol': 'socks',
+                'settings': {
+                    'auth': 'noauth',
+                    'udp': False,
+                    'ip': '127.0.0.1',
+                    'clients': None
+                },
+                'streamSettings': None
+            },
+            'outbound': {
+                'tag': 'agentout',
+                'protocol': 'vmess',
+                'settings': {
+                    'vnext': [
+                        {
+                            'address': '213.213.213.213',
+                            'port': 23333,
+                            'users': [
+                                {
+                                    'id': 'dddda000-bbbb-4444-2222-fffff6666666',
+                                    'alterId': 100,
+                                    'security': 'aes-128-gcm'
+                                }
+                            ]
+                        }
+                    ],
+                    'servers': None
+                },
+                'streamSettings': {
+                    'network': None,
+                    'security': None,
+                    'tcpSettings': None,
+                    'kcpSettings': None,
+                    'wsSettings': None
+                },
+                'mux': {
+                    'enabled': False
                 }
             },
-            "log": {
-                "access": "",
-                "error": "",
-                "loglevel": "warning"
-            },
-            "inbounds": [
+            'inboundDetour': None,
+            'outboundDetour': [
                 {
-                    "port": 1086,
-                    "listen": "127.0.0.1",
-                    "protocol": "socks",
-                    "settings": {
-                        "auth": "noauth",
-                        "udp": True,
+                    'protocol': 'freedom',
+                    'settings': {
+                        'response': None
                     },
-                    "tag": "proxy",
-                    "sniffing": {
-                        "enabled": True,
-                        "destOverride": [
-                            "http",
-                            "tls"
-                        ]
-                    },
-                }
-            ],
-            "outbounds": [
-                {
-                    "settings": {},
-                    "protocol": "freedom",
-                    "tag": "direct"
+                    'tag': 'direct'
                 },
                 {
-                    "settings": {},
-                    "protocol": "blackhole",
-                    "tag": "blocked"
+                    'protocol': 'blackhole',
+                    'settings': {
+                        'response': {
+                            'type': 'http'
+                        }
+                    },
+                    'tag': 'blockout'
                 }
             ],
-            "routing": {
-                "domainStrategy": "IPIfNonMatch",
-                "rules": [
-                    {
-                        "type": "field",
-                        "port": None,
-                        "inboundTag": "api",
-                        "outboundTag": "api",
-                        "ip": None,
-                        "domain": None
-                    },
-                    {
-                        "type": "field",
-                        "port": None,
-                        "inboundTag": None,
-                        "outboundTag": "proxy",
-                        "ip": None,
-                        "domain": [
-                            "geosite:google",
-                            "geosite:github",
-                            "geosite:netflix",
-                            "geosite:steam",
-                            "geosite:telegram",
-                            "geosite:tumblr",
-                            "geosite:speedtest",
-                            "geosite:bbc",
-                            "domain:gvt1.com",
-                            "domain:textnow.com",
-                            "domain:twitch.tv",
-                            "domain:wikileaks.org",
-                            "domain:naver.com",
-                            "domain:h28o.com",
-                            "domain:haitum.com",
-                            "domain:99deo.com/",
-                            "domain:sukebei.nyaa.si",
-                            "domain:fanqiangdang.com"
-                        ]
-                    },
-                    {
-                        "type": "field",
-                        "port": None,
-                        "inboundTag": None,
-                        "outboundTag": "proxy",
-                        "ip": [
-                            "91.108.4.0/22",
-                            "91.108.8.0/22",
-                            "91.108.12.0/22",
-                            "91.108.20.0/22",
-                            "91.108.36.0/23",
-                            "91.108.38.0/23",
-                            "91.108.56.0/22",
-                            "149.154.160.0/20",
-                            "149.154.164.0/22",
-                            "149.154.172.0/22",
-                            "74.125.0.0/16",
-                            "173.194.0.0/16",
-                            "172.217.0.0/16",
-                            "216.58.200.0/24",
-                            "216.58.220.0/24"
-                        ],
-                        "domain": None
-                    },
-                    {
-                        "type": "field",
-                        "port": None,
-                        "inboundTag": None,
-                        "outboundTag": "direct",
-                        "ip": None,
-                        "domain": [
-                            "domain:12306.com",
-                            "domain:51ym.me",
-                            "domain:52pojie.cn",
-                            "domain:8686c.com",
-                            "domain:abercrombie.com",
-                            "domain:adobesc.com",
-                            "domain:air-matters.com",
-                            "domain:air-matters.io",
-                            "domain:airtable.com",
-                            "domain:akadns.net",
-                            "domain:apache.org",
-                            "domain:api.crisp.chat",
-                            "domain:api.termius.com",
-                            "domain:appshike.com",
-                            "domain:appstore.com",
-                            "domain:aweme.snssdk.com",
-                            "domain:bababian.com",
-                            "domain:battle.net",
-                            "domain:beatsbydre.com",
-                            "domain:bet365.com",
-                            "domain:bilibili.cn",
-                            "domain:ccgslb.com",
-                            "domain:ccgslb.net",
-                            "domain:chunbo.com",
-                            "domain:chunboimg.com",
-                            "domain:clashroyaleapp.com",
-                            "domain:cloudsigma.com",
-                            "domain:cloudxns.net",
-                            "domain:cmfu.com",
-                            "domain:culturedcode.com",
-                            "domain:dct-cloud.com",
-                            "domain:didialift.com",
-                            "domain:douyutv.com",
-                            "domain:duokan.com",
-                            "domain:dytt8.net",
-                            "domain:easou.com",
-                            "domain:ecitic.net",
-                            "domain:eclipse.org",
-                            "domain:eudic.net",
-                            "domain:ewqcxz.com",
-                            "domain:fir.im",
-                            "domain:frdic.com",
-                            "domain:fresh-ideas.cc",
-                            "domain:godic.net",
-                            "domain:goodread.com",
-                            "domain:haibian.com",
-                            "domain:hdslb.net",
-                            "domain:hollisterco.com",
-                            "domain:hongxiu.com",
-                            "domain:hxcdn.net",
-                            "domain:images.unsplash.com",
-                            "domain:img4me.com",
-                            "domain:ipify.org",
-                            "domain:ixdzs.com",
-                            "domain:jd.hk",
-                            "domain:jianshuapi.com",
-                            "domain:jomodns.com",
-                            "domain:jsboxbbs.com",
-                            "domain:knewone.com",
-                            "domain:kuaidi100.com",
-                            "domain:lemicp.com",
-                            "domain:letvcloud.com",
-                            "domain:lizhi.io",
-                            "domain:localizecdn.com",
-                            "domain:lucifr.com",
-                            "domain:luoo.net",
-                            "domain:mai.tn",
-                            "domain:maven.org",
-                            "domain:miwifi.com",
-                            "domain:moji.com",
-                            "domain:moke.com",
-                            "domain:mtalk.google.com",
-                            "domain:mxhichina.com",
-                            "domain:myqcloud.com",
-                            "domain:myunlu.com",
-                            "domain:netease.com",
-                            "domain:nfoservers.com",
-                            "domain:nssurge.com",
-                            "domain:nuomi.com",
-                            "domain:ourdvs.com",
-                            "domain:overcast.fm",
-                            "domain:paypal.com",
-                            "domain:paypalobjects.com",
-                            "domain:pgyer.com",
-                            "domain:qdaily.com",
-                            "domain:qdmm.com",
-                            "domain:qin.io",
-                            "domain:qingmang.me",
-                            "domain:qingmang.mobi",
-                            "domain:qqurl.com",
-                            "domain:rarbg.to",
-                            "domain:rrmj.tv",
-                            "domain:ruguoapp.com",
-                            "domain:sm.ms",
-                            "domain:snwx.com",
-                            "domain:soku.com",
-                            "domain:startssl.com",
-                            "domain:store.steampowered.com",
-                            "domain:symcd.com",
-                            "domain:teamviewer.com",
-                            "domain:tmzvps.com",
-                            "domain:trello.com",
-                            "domain:trellocdn.com",
-                            "domain:ttmeiju.com",
-                            "domain:udache.com",
-                            "domain:uxengine.net",
-                            "domain:weather.bjango.com",
-                            "domain:weather.com",
-                            "domain:webqxs.com",
-                            "domain:weico.cc",
-                            "domain:wenku8.net",
-                            "domain:werewolf.53site.com",
-                            "domain:windowsupdate.com",
-                            "domain:wkcdn.com",
-                            "domain:workflowy.com",
-                            "domain:xdrig.com",
-                            "domain:xiaojukeji.com",
-                            "domain:xiaomi.net",
-                            "domain:xiaomicp.com",
-                            "domain:ximalaya.com",
-                            "domain:xitek.com",
-                            "domain:xmcdn.com",
-                            "domain:xslb.net",
-                            "domain:xteko.com",
-                            "domain:yach.me",
-                            "domain:yixia.com",
-                            "domain:yunjiasu-cdn.net",
-                            "domain:zealer.com",
-                            "domain:zgslb.net",
-                            "domain:zimuzu.tv",
-                            "domain:zmz002.com",
-                            "domain:samsungdm.com"
-                        ]
-                    },
-                    {
-                        "type": "field",
-                        "port": None,
-                        "inboundTag": None,
-                        "outboundTag": "block",
-                        "ip": None,
-                        "domain": [
-                            "geosite:category-ads"
-                        ]
-                    },
-                    {
-                        "type": "field",
-                        "port": None,
-                        "inboundTag": None,
-                        "outboundTag": "direct",
-                        "ip": [
-                            "geoip:private"
-                        ],
-                        "domain": None
-                    },
-                    {
-                        "type": "field",
-                        "port": None,
-                        "inboundTag": None,
-                        "outboundTag": "direct",
-                        "ip": [
-                            "geoip:cn"
-                        ],
-                        "domain": None
-                    },
-                    {
-                        "type": "field",
-                        "port": None,
-                        "inboundTag": None,
-                        "outboundTag": "direct",
-                        "ip": None,
-                        "domain": [
-                            "geosite:cn"
-                        ]
-                    }
+            '_dns': {
+                'servers': [
+                    '8.8.8.8',
+                    '8.8.4.4',
+                    '114.114.114.114'
                 ]
+            },
+            '_routing': {
+                'strategy': 'rules',
+                'settings': {
+                    'domainStrategy': 'IPIfNonMatch',
+                    'rules': [
+                        {
+                            'type': 'field',
+                            'port': None,
+                            'outboundTag': 'direct',
+                            'ip': [
+                                '0.0.0.0/8',
+                                '10.0.0.0/8',
+                                '100.64.0.0/10',
+                                '127.0.0.0/8',
+                                '169.254.0.0/16',
+                                '172.16.0.0/12',
+                                '192.0.0.0/24',
+                                '192.0.2.0/24',
+                                '192.168.0.0/16',
+                                '198.18.0.0/15',
+                                '198.51.100.0/24',
+                                '203.0.113.0/24',
+                                '::1/128',
+                                'fc00::/7',
+                                'fe80::/10'
+                            ],
+                            'domain': None
+                        }
+                    ]
+                }
             }
         }
+
         if self.network == 'tcp' or self.network == 'auto':
             # tcp下
             v2rayConf['outbounds'].append({
@@ -457,8 +275,8 @@ class Shadowsocks(Node):
     def formatConfig(self):
         ssConfig = {
             "log": {
-                "access" : "/var/log/v2ray/access.log",
-                "error":"/var/log/v2ray/error.log",
+                "access": "/var/log/v2ray/access.log",
+                "error": "/var/log/v2ray/error.log",
                 "logLevel": "none"
             },
             "inbounds": [
@@ -500,7 +318,7 @@ class Shadowsocks(Node):
                         {
                             "type": "field",
                             "inboundTag": ["in"],
-                            "outboundTag":"out"
+                            "outboundTag": "out"
                         }
                     ]
                 }
