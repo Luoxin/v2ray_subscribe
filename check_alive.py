e08ba9ffa4b8114d67507a6cf886944cddbeda0a
luoxin <luoxin.ttt@gmail.com>
2019-10-20 16:32:30 +0800

    更改测速的超时时间

--- check_alive.py
+++ check_alive.py
@@ -72,7 +72,7 @@ def check_by_v2ray_url(url: str) -> float:
             }
             r = requests.get(TEST_FILE_URL,
                              proxies=PROXIES_TEST,
-                             timeout=5 * 1000,
+                             timeout=1 * 1000,
                              headers=headers
                              )
             if r.status_code == 200:
