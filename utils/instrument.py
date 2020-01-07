37ff536fbb031d2484486f53e6454b996b077b52
LuoxinY <luoxin.ttt@gmail.com>
2020-01-07 18:49:41 +0800

    base64 encode

--- utils/instrument.py
+++ utils/instrument.py
@@ -1,36 +1,39 @@
+import base64
 import hashlib
 import json
 import uuid as uu
 
 
-class Instrument:
-    @staticmethod
-    def is_json(data: (bytes, str)) -> bool:
-        if isinstance(data, bytes):
-            data = data.decode()
-        if isinstance(data, str):
-            try:
-                json.loads(data)
-                return True
-            except:
-                return False
-        return False
+def is_json(data: (bytes, str)) -> bool:
+    if isinstance(data, bytes):
+        data = data.decode()
+    if isinstance(data, str):
+        try:
+            json.loads(data)
+            return True
+        except:
+            return False
+    return False
 
-    @staticmethod
-    def encode_to_utf8(data: str != "") -> str:
-        # chardet.detect(data.encode()).get("encoding")
-        return data.encode().decode("utf-8")
 
-    @staticmethod
-    def gen_uuid() -> str:
-        """
-            获取uuid
-        :return: uuid
-        """
-        return uu.uuid4().hex
+def encode_to_utf8(data: str != "") -> str:
+    # chardet.detect(data.encode()).get("encoding")
+    return data.encode().decode("utf-8")
 
-    @staticmethod
-    def md5(s) -> str:
-        hl = hashlib.md5()
-        hl.update(s.encode(encoding='utf-8'))
-        return hl.hexdigest()
+
+def gen_uuid() -> str:
+    """
+        获取uuid
+    :return: uuid
+    """
+    return uu.uuid4().hex
+
+
+def md5(s) -> str:
+    hl = hashlib.md5()
+    hl.update(s.encode(encoding='utf-8'))
+    return hl.hexdigest()
+
+
+def base64_encode(s) -> str:
+    return base64.b64encode(s.encode()).decode("utf-8")
