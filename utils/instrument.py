625e42de2558d48c8876b113b1d383b196889bfd
LuoxinY <luoxin.ttt@gmail.com>
2020-01-08 13:03:16 +0800

    base64 encode 添加对json的支持，并限制了encode的类型
    添加了对 base64 decode 的支持

--- utils/instrument.py
+++ utils/instrument.py
@@ -1,7 +1,6 @@
 import base64
 import hashlib
 import json
-import uuid as uu
 
 
 def is_json(data: (bytes, str)) -> bool:
@@ -35,5 +34,19 @@ def md5(s) -> str:
     return hl.hexdigest()
 
 
-def base64_encode(s) -> str:
+def base64_encode(s: (str, int, float, dict)) -> str:
+    if isinstance(s, str):
+        pass
+    elif isinstance(s, (int, float)):
+        s = str(s)
+    elif isinstance(s, dict):
+        s = json.dumps(s)
+
     return base64.b64encode(s.encode()).decode("utf-8")
+
+def base64_decode(base64_str):
+    base64_str = base64_str.replace("\n", "").replace("-", "+").replace("_", "/")
+    padding = int(len(base64_str) % 4)
+    if padding != 0:
+        base64_str += "=" * (4 - padding)
+    return str(base64.b64decode(base64_str), "utf-8")
