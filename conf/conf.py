069a2a8f5b827982530f3073e23a4e5d2499e8b1
luoxin <luoxin.ttt@gmail.com>
2020-01-10 08:32:03 +0800

    style

--- conf/conf.py
+++ conf/conf.py
@@ -3,11 +3,12 @@ import yaml, os
 global s
 
 
-def init(filename=os.path.abspath(os.path.dirname(__file__))+"conf.yaml"):
+def init(filename=os.path.abspath(os.path.dirname(__file__)) + "conf.yaml"):
     filename = "test.yaml"
 
     f = open(filename, encoding="utf-8")
     y = yaml.load(f, Loader=yaml.FullLoader)
+
 
 if __name__ == '__main__':
     print(init())
