5813c0939f2dc65db5588c3d0eb86a3fd0fd2b3d
LuoxinY <luoxin.ttt@gmail.com>
2020-01-20 11:13:49 +0800

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="v2ray_subscribe",  # 包名字
+    version="0.0.0.2",  # 包版本
+    description="Grab and detect available over the wall nodes",  # 简单描述
+    author="Luoxin",  # 作者
+    author_email="luoxin.ttt@gmail.com",  # 作者邮箱
+    url="https://gitee.com/luoxinY/v2ray_subscribe",  # 包的主页
+    packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
+)
